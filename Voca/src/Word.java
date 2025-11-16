d7cb9910e1d2a6a761acdc38a88e4c10e7249676
DEV-salmon <hsb11250@gmail.com>
2025-11-16 13:53:06 +0900

    Revert "Add files via upload"
    This reverts commit 834deeaee53aa6cb494fc4f7221321cf053ce0b5.

--- Voca/src/Word.java
+++ Voca/src/Word.java
@@ -1,38 +1,34 @@
-package teamProject;
-
 public class Word {
     private String eng;
-    private String kor;
+    private String[] kor;
     private String ex;
 
-    public Word(String eng, String kor, String ex) {
+    public Word(String eng, String[] kor){
         this.eng = eng;
         this.kor = kor;
+    }
+    public Word(String eng, String[] kor, String ex){
+        this(eng, kor);
         this.ex = ex;
     }
-
+    public String getEx() {
+        return ex;
+    }
     public String getEng() {
         return eng;
     }
 
-    public String getKor() {
+    public String[] getKor() {
         return kor;
     }
-
-    public void setEng(String eng) {
-        this.eng = eng;
-    }
-
-    public void setKor(String kor) {
-        this.kor = kor;
-    }
-
     public void setEx(String ex) {
         this.ex = ex;
     }
-
-    @Override
-    public String toString() {
-        return this.eng + " : " + kor + "   ex. " + ex;
+    public void setEng(String eng) {
+        this.eng = eng;
     }
+    public void setKor(String[] kor) {
+        this.kor = kor;
+    }
+
 }
